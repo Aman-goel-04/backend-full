75503e9c44826f9f0196ffdf9fb7d56dbe9fcb14
Aman Goel <amangoel505@gmail.com>
2026-01-23 01:30:50 +0530

    Implement user account management features: change password, fetch current user, update account details, and manage user avatars and cover images

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -254,11 +254,138 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
             "Invalid refresh token."
         );
     }
-})
+});
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const {oldPassword, newPassword, confirmPassword} = req.body
+
+    if(newPassword !== confirmPassword){
+        throw new ApiError(401, "New password and confirm password do not match.")
+    }
+
+    // agar middleware chala hai matlab req.user mein user hai
+    const user = await User.findById(req.user._id);
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "Invalid old password.");
+    }
+
+    // now i need to set the new password
+    // pre wala middleware chalega user model mein (VV IMP)
+
+    user.password = newPassword;
+    await user.save({validateBeforeSave: false});
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "Password changed successfully."
+        )
+    )
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res
+    .status(200)
+    .json(200, req.user, "Current user fetched successfully.");
+});
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    const {fullName, email} = req.body;
+
+    if(!fullName && !email){
+        throw new ApiError(400, "All fields are required.");
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id, // find this
+        {
+            $set: {
+                fullName,
+                email: email
+            }
+        }, 
+        {new: true} // update hone ke baad wali info return hoti hai
+    ).select("-password");
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully."));
+});
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+    const avatarLocalPath = req.file?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is missing.");
+    }
+
+    const avatarReceived = await uploadOnCloudinary(avatarLocalPath); 
+
+    if(!avatarReceived.url){
+        throw new ApiError(400, "Error while uploading on cloudinary.");
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.users?._id,
+        {
+            $set:{
+                avatar: avatarReceived.url // avatar recieved is a complete object
+            }
+        },
+        {new: true}
+    ).select("-password");
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Avatar image updated successfully.")
+    )
+});
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+    const coverImageLocalPath = req.file?.path; // note: file hoga, not files... 
+
+    if(!coverImageLocalPath){
+        throw new ApiError(400, "Cover file is missing.");
+    }
+
+    const coverImageReceived = await uploadOnCloudinary(coverImageLocalPath); 
+
+    if(!coverImageReceived.url){
+        throw new ApiError(400, "Error while uploading on cloudinary.");
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.users?._id,
+        {
+            $set:{
+                avatar: coverImageReceived.url // avatar recieved is a complete object
+            }
+        },
+        {new: true}
+    ).select("-password");
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Avatar image updated successfully.")
+    )
+});
+
 
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser, 
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
 };
