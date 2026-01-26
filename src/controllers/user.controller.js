008af7467b2a6fb36c6e035c3cba5804fb519cdd
Aman Goel <amangoel505@gmail.com>
2026-01-26 13:06:01 +0530

    Add getUserChannelProfile endpoint to fetch user channel details with subscription info

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import { subscribe } from "diagnostics_channel";
 
 const generateAccessAndRefreshToken = async(userId) => {
     try {
@@ -383,6 +384,78 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     )
 });
 
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+    const {username} = req.params; // channel ko url se lena hai
+
+    if(!username?.trim()){
+        throw new ApiError(400, "Username is missing.");
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers" 
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo" 
+            }
+        },
+        {
+            $addFields: {
+                // additional fields
+                subscribersCount: {
+                    $size: "$subscribers" // size is used to count 
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1, 
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1, 
+                isSubscribed: 1, 
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+                // createdAt could be sent if i wanna show the "channel created 5 years ago"
+            }
+        }
+    ]);
+
+    if(!channel.length){
+        throw new ApiError(404, "Channel does not exist.");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0], "User channel fetched successfully.")
+    )
+});
 
 export {
     registerUser,
